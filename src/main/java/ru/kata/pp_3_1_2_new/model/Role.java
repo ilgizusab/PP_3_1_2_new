c9fa28cbea08d9e35621f1c8a9d1187a3f32d2ed
ILGIZ SAB <ilgizusab@mail.ru>
2024-12-22 23:21:02 +0300

    221224

--- src/main/java/ru/kata/pp_3_1_2_new/model/Role.java
+++ src/main/java/ru/kata/pp_3_1_2_new/model/Role.java
@@ -1,17 +1,24 @@
 package ru.kata.pp_3_1_2_new.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
 import java.util.Objects;
 
 @Entity
 @Table(name = "roles")
-public class Role {
+public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true, nullable = false)
     private String name;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
 
     public Long getId() {
         return id;
